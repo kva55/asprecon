76abd28b5bad3c2a527c590ecace35fd28119ad0
kvass <60018788+kva55@users.noreply.github.com>
2023-07-26 00:42:40 -0600

    Update asprecon.py

--- asprecon.py
+++ asprecon.py
@@ -1,6 +1,8 @@
-import requests, argparse, sys, time
+import requests, argparse, sys, time, urllib3
 from termcolor import colored
 
+# Disable warning about SSL, comment if needed
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # global vars
 delay = 0
@@ -12,22 +14,35 @@ file.close()
 
 
 def CheckDirectory(target):
-    print("\nChecking if target is vulnerable.\n")
-    r = requests.get(url="%s/aspnet_client/" % target)
-    if str(r.status_code) == "403" or str(r.status_code) == "403.14":
-        print(colored("Target is likely vulnerable.", "green"))
-        return True
-    
-    else:
-        print(colored("Target is likely not vulnerable.\n", "red"))
-        return False
+    try:
+        print("\nChecking if target is vulnerable.\n")
+        r = requests.get(url="%s/aspnet_client/" % target, verify=False)
+
+        if str(r.status_code) == "403" or str(r.status_code) == "403.14":
+            r = requests.get(url="%s/aspnet_clien/" % target, verify=False)
+
+            if str(r.status_code) == "403" or str(r.status_code) == "403.14":
+                print(colored("Target is likely not vulnerable.\n", "red"))
+                return False
+
+            else:
+                print(colored("Target is likely vulnerable.", "green"))
+                return True
+        
+        else:
+            print(colored("Target is likely not vulnerable.\n", "red"))
+            return False
+        
+    except Exception as e:
+        print(e)
+        exit(1)
 
 def BruteForce(target):
     for version in versions:
         version = requests.utils.quote(version.strip())
 
         if delay <= 0:
-            a = requests.get(url="%s/aspnet_client/system_web/%s/" % (target,str(version)))
+            a = requests.get(url="%s/aspnet_client/system_web/%s/" % (target,str(version)), verify=False)
             
             if str(a.status_code) == "403" or str(a.status_code) == "403.14":
                 print(colored("Asp.Net version: " + version.replace("_","."), "yellow"))
@@ -36,7 +51,7 @@ def BruteForce(target):
                 continue
         else:
             time.sleep(delay)
-            a = requests.get(url="%s/aspnet_client/system_web/%s/" % (target,str(version)))
+            a = requests.get(url="%s/aspnet_client/system_web/%s/" % (target,str(version)), verify=False)
             if str(a.status_code) == "403" or str(a.status_code) == "403.14":
                 print(colored("Asp.Net version: " + version.replace("_","."), "yellow"))
                         
@@ -67,3 +82,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
