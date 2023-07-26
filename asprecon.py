34396373e1d1bff0660dd45ed7ca3aeb2dd61821
kvass <60018788+kva55@users.noreply.github.com>
2023-07-25 23:25:38 -0600

    Update asprecon.py

--- asprecon.py
+++ asprecon.py
@@ -13,13 +13,13 @@ file.close()
 
 def CheckDirectory(target):
     print("\nChecking if target is vulnerable.\n")
-    r = requests.get(url="http://%s/aspnet_client/" % target)
+    r = requests.get(url="%s/aspnet_client/" % target)
     if str(r.status_code) == "403" or str(r.status_code) == "403.14":
         print(colored("Target is likely vulnerable.", "green"))
         return True
     
     else:
-        print("Target is likely not vulnerable.\n")
+        print(colored("Target is likely not vulnerable.\n", "red"))
         return False
 
 def BruteForce(target):
@@ -27,7 +27,7 @@ def BruteForce(target):
         version = requests.utils.quote(version.strip())
 
         if delay <= 0:
-            a = requests.get(url="http://%s/aspnet_client/system_web/%s/" % (target,str(version)))
+            a = requests.get(url="%s/aspnet_client/system_web/%s/" % (target,str(version)))
             
             if str(a.status_code) == "403" or str(a.status_code) == "403.14":
                 print(colored("Asp.Net version: " + version.replace("_","."), "yellow"))
@@ -36,7 +36,7 @@ def BruteForce(target):
                 continue
         else:
             time.sleep(delay)
-            a = requests.get(url="http://%s/aspnet_client/system_web/%s/" % (target,str(version)))
+            a = requests.get(url="%s/aspnet_client/system_web/%s/" % (target,str(version)))
             if str(a.status_code) == "403" or str(a.status_code) == "403.14":
                 print(colored("Asp.Net version: " + version.replace("_","."), "yellow"))
                         
@@ -47,9 +47,10 @@ def main():
 
     global delay
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--target", help="Target to scan (ip/domain)", required=True)
-    parser.add_argument("-d", "--delay",type=int, help="Time delay each scan per second(s)")
+    parser = argparse.ArgumentParser(epilog="e.g. python3 asprecon -t http/s://127.0.0.1:80 -d 1",)
+    parser.add_argument("-t", "--target", metavar='', help="Target to scan (ip/domain)", required=True)
+    parser.add_argument("-d", "--delay",metavar='', type=int, help="Time delay each scan per second(s)")
+                        
     args = parser.parse_args()
 
     target = args.target
